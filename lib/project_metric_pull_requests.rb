72cfc3ed6a6885c12d54f4b4cea68a550f561c47
an-ju <an_ju@berkeley.edu>
2017-12-19 12:19:26 -0800

    fix bug

--- lib/project_metric_pull_requests.rb
+++ lib/project_metric_pull_requests.rb
@@ -1,6 +1,7 @@
 require "project_metric_pull_requests/version"
 require 'octokit'
 require 'json'
+require 'open-uri'
 
 class ProjectMetricPullRequests
   attr_accessor :raw_data
@@ -8,6 +9,7 @@ class ProjectMetricPullRequests
   def initialize(credentials = {}, raw_data = nil)
     @url = credentials[:github_project]
     @identifier = URI::parse(@url).path[1..-1]
+    @token = credentials[:github_access_token]
     @client = Octokit::Client.new access_token: credentials[:github_access_token]
     @client.auto_paginate = true
 
@@ -19,14 +21,14 @@ class ProjectMetricPullRequests
     @image ||= { chartType: 'pull_requests',
                  titleText: 'Pull Request Status',
                  data: { total: @raw_data.length,
-                         open: @raw_data.select { |pr| pr[:state].eql? 'open' }.length,
-                         closed: @raw_data.select { |pr| pr[:state].eql? 'closed' }.length,
-                         commented: @raw_data.reject { |pr| pr_comments(pr).empty? }.length } }.to_json
+                         open: @raw_data.select { |pr| pr['state'].eql? 'open' }.length,
+                         closed: @raw_data.select { |pr| pr['state'].eql? 'closed' }.length,
+                         commented: @raw_data.select { |pr| num_comments(pr) > 0 }.length } }.to_json
   end
 
   def score
     @raw_data ||= pull_requests
-    @score ||= @raw_data.length > 0 ? @raw_data.reject { |pr| pr_comments(pr).empty? }.length / @raw_data.length : 0.0
+    @score ||= @raw_data.length > 0 ? @raw_data.select { |pr| num_comments(pr) > 0 }.length.to_f / @raw_data.length.to_f : 0.0
   end
 
   def refresh
@@ -41,10 +43,12 @@ class ProjectMetricPullRequests
   private
 
   def pull_requests
-    @client.pull_requests @identifier, state: 'all'
+    @client.pull_requests( @identifier, state: 'all').map do |pr|
+      JSON.load(open(pr[:url], 'Authorization' => "token #{@token}"))
+    end
   end
 
-  def pr_comments(pr)
-    @client.pull_request_comments @identifier, pr[:number]
+  def num_comments(pr)
+    pr['comments'] + pr['review_comments']
   end
 end
